a307ca5764d094b16ad602e55a162c0618a8f55f
Nicholas Avenell <nicholas@istic.net>
2026-05-08 05:18:34 +0000

    Force https on prod

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+
+        if ($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
         $this->configureDefaults();
     }
 
